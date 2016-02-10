2bde4ae0abbd5993e8cf7e599ff14e8233befec2
K Lauer <klauer@bnl.gov>
2016-02-10 15:26:24 -0500

    Add examples and images

--- examples/motor.py
+++ examples/motor.py
@@ -20,18 +20,3 @@ print('derivative gain metadata', metadata['derivative_gain'])
 print('in links', list(rec.attrs_of_type('DBF_INLINK')))
 print('all links', list(rec.attrs_of_type(['DBF_INLINK', 'DBF_OUTLINK',
                                            'DBF_FWDLINK'])))
-
-from recordwhat.graph import graph_links
-
-graph = graph_links('XF:31IDA-OP{Tbl-Ax:FakeMtr}-SP_')
-# or a number of records in one diagram:
-# graph = graph_links('XF:03IDC-ES{SPod:1}Move-Cmd',
-#                     'XF:03IDC-ES{SPod:1}Move-Cmd_',
-#                     'XF:03IDC-ES{SPod:1}Moving-I_',
-#                     'XF:03IDC-ES{SPod:1}MovingRaw-I_',
-#                     'XF:03IDC-ES{SPod:1}Moving-I',
-#                     'XF:03IDC-ES{SPod:1}Move-Seq_',
-#                     'XF:03IDC-ES{SPod:1}BusyMoving_',
-#                     )
-
-print('rendered graph to', graph.render('test'))
