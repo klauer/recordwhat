c46ff8cbc52662b6ce161c43337d59fd99635214
K Lauer <klauer@bnl.gov>
2016-06-23 13:21:07 -0400

    Add version variable

--- recordwhat/__init__.py
+++ recordwhat/__init__.py
@@ -1,3 +1,7 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 from .record_base import (RecordBase, FieldComponent)
 from .registry import (get_record_class, _register_record_type,
                        get_record_by_name)
