1e2bb867d2450e4f9a82a0d150eb9ae6b4f496a9
Thomas A Caswell <tcaswell@bnl.gov>
2017-03-21 22:00:56 -0400

    ENH: tweak the grammar + write table like dbd.py

--- recordwhat/parsers/dbd_parsimonious.py
+++ recordwhat/parsers/dbd_parsimonious.py
@@ -1,25 +1,32 @@
 from parsimonious.grammar import Grammar, NodeVisitor
+from collections import defaultdict
+import os.path
 
-dbd_grammer = Grammar(r"""
+dbd_grammar = Grammar(r"""
 dbd = (comment / cimport / field / menu / record_type /
        variable / device / include / registrar /
        function / driver / "\n")+
 
-field = _ "field(" f_name "," _ f_type ")" _ "{" (fp / "\n" )* _  "}"
+field = _ "field(" f_name "," _ f_type ")" _ "{" field_body _  "}"
+field_body = fp*
 f_name = ~"[A-Z0-9_]+"
 f_type = ("DBF_STRING" / "DBF_CHAR" / "DBF_UCHAR" /
           "DBF_SHORT" / "DBF_USHORT" / "DBF_LONG" / "DBF_ULONG" /
           "DBF_FLOAT" / "DBF_DOUBLE" / "DBF_ENUM" / "DBF_MENU" /
           "DBF_DEVICE" / "DBF_INLINK" / "DBF_OUTLINK" / "DBF_FWDLINK" /
           "DBF_NOACCESS")
-fp = _ (prompt / special / size / promptgroup /
-        g_field / comment / extra / "\n")
+fp = _ (g_field / prompt / special / size / promptgroup /
+        comment / extra / "\n")
+g_field = g_f_name "(" g_f_body ")\n"
+
 prompt = "prompt(" prompt_val ")\n"
 special = "special(" special_val ")\n"
 size = "size(" size_val ")\n"
 extra = "extra(" extra_val ")\n"
-g_field = ~"[a-z]*\(" ~"[^)]*" ")\n"
 promptgroup = "promptgroup(" promptgroup_val ")\n"
+
+g_f_name = ~"[a-z]*"
+g_f_body = ~"[^)]*"
 prompt_val = ~'"[^"]*"'
 extra_val = ~'"[^"]*"'
 promptgroup_val = ~'"[^"]*"'
@@ -49,43 +56,62 @@ comment = ~"\s*#[^\r\n]*"
 """)
 
 
-class OphydMaker(NodeVisitor):
+class TableMaker(NodeVisitor):
+    def __init__(self, *, out_path='/tmp'):
+        self.out_path = out_path
 
     def visit_dbd(self, node, visited_children):
         ...
 
     def visit_field(self, node, visited_children):
-        ...
+        (_ws, _kw, f_name, _cm, _ws, f_type, _rp, _ws, _lc,
+         fp, _ws, _rc) = visited_children
+        return (f_name, f_type,
+                defaultdict(str, {k: v for ((k, v),) in fp}))
 
     def visit_f_name(self, node, visited_children):
-        ...
+        return node.text
+
+    def visit_field_body(self, node, visited_children):
+        return visited_children
 
     def visit_f_type(self, node, visited_children):
-        ...
+        return node.text
+
+    def visit_g_f_name(self, node, visited_children):
+        return node.text
+
+    def visit_g_f_body(self, node, visited_children):
+        return node.text
 
     def visit_fp(self, node, visited_children):
-        ...
+        _ws, p = visited_children
+        return p
 
     def visit_prompt(self, node, visited_children):
-        ...
+        _, pv, *_ = visited_children
+        return ('prompt', pv)
 
     def visit_special(self, node, visited_children):
-        ...
+        _, sz, _ = visited_children
+        return ('special', sz)
 
     def visit_size(self, node, visited_children):
-        ...
+        _, sz, _ = visited_children
+        return ('size', sz)
 
     def visit_extra(self, node, visited_children):
         ...
 
     def visit_g_field(self, node, visited_children):
-        ...
+        g_nm, _lp, g_b, _rp = visited_children
+        return (g_nm, g_b)
 
     def visit_promptgroup(self, node, visited_children):
         ...
 
     def visit_prompt_val(self, node, visited_children):
-        ...
+        return node.text
 
     def visit_extra_val(self, node, visited_children):
         ...
@@ -109,7 +135,20 @@ class OphydMaker(NodeVisitor):
         ...
 
     def visit_record_type(self, node, visited_children):
-        ...
+        _kw, name, _rp, _ws, _lc, innards, _rc = visited_children
+        fields = sorted([_[0] for _ in innards
+                         if _[0] is not None and len(_[0])])
+
+        columns = ['field', 'type', 'asl', 'initial', 'promptgroup',
+                   'prompt', 'special', 'pp', 'interest', 'base', 'size',
+                   'extra', 'menu']
+
+        fn = os.path.join(self.out_path, '{}.txt'.format(name))
+        with open(fn, 'w') as fout:
+            print('\t'.join(columns), file=fout)
+            for nm, typ, md in fields:
+                row = [nm, typ] + [md[k] for k in columns[2:]]
+                print('\t'.join(row), file=fout)
 
     def visit_include(self, node, visited_children):
         ...
@@ -118,7 +157,7 @@ class OphydMaker(NodeVisitor):
         ...
 
     def visit_rec_name(self, node, visited_children):
-        ...
+        return node.text
 
     def visit__(self, node, visited_children):
         ...
@@ -148,4 +187,4 @@ class OphydMaker(NodeVisitor):
         ...
 
     def visit_(self, node, visited_children):
-        ...
+        return visited_children
