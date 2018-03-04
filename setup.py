37ef4764e5a5fe923fa8b1938b871292da438cf0
K Lauer <kenneth.lauer@gmail.com>
2018-03-04 13:04:56 -0800

    BLD: fix dependencies (pandas, pyepics)

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ import versioneer
 setup(name='recordwhat',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      install_requires=['ophyd', 'parsimonious',
-                        'graphviz', 'attrs'],
+      install_requires=['ophyd', 'parsimonious', 'graphviz', 'attrs', 'pandas',
+                        'pyepics',
+                        ],
       license='BSD',
       packages=find_packages())
